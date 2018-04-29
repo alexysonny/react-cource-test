6b6e00ffa1635d920cd20bad16541ed04ebf6e68
Aleksei <alexysonny@gmail.com>
2018-04-29 23:50:58 +0300

    bilerplate created

---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component{
+    render(){
+        return(
+            <div className="navbarWrapper">
+                <h1>Hello Component</h1>
+            </div>
+        );
+    }
+}
