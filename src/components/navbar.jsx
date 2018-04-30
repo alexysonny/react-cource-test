2869701b30f0652a78bb1456318f3d151af09e58
Aleksei Kostenko <alexysonny@gmail.com>
2018-04-30 16:26:51 +0300

    add some styles to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import './navbar.css'
 
 export default class Navbar extends Component{
     render(){
         return(
             <div className="navbarWrapper">
-                <h1>Hello Component</h1>
+                <div className="navbarElements">
+                    <ul>
+                        <li>Домой</li>
+                        <li>Новости</li>
+                        <li>Профиль</li>
+                        <li>Вход</li>
+                    </ul>
+                </div>
             </div>
         );
     }
