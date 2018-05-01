ae28cedb84796bfe79fa8622b50f6cd29f46de3e
Aleksei <alexysonny@gmail.com>
2018-05-02 00:11:56 +0300

    add inputs and button event handlers at login page

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -2,20 +2,49 @@ import React, { Component } from "react";
 import "./login.css";
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      login: "",
+      password: ""
+    };
+    this.onSubmitHandler = this.onSubmitHandler.bind(this)
+  }
+  onChangeHandler(fieldName, e) {
+    console.log(e.target.value);
+    if (e.target.value.trim().length > 0) {
+      this.setState({ ["" + fieldName]: e.target.value });
+    }
+  }
+  onSubmitHandler() {
+    if (this.state.login === 'Admin' && this.state.password === '12345') {
+      localStorage.setItem('Authorised', true)
+    } else {
+      localStorage.setItem('Authorised', false)
+    }
+  }
   render() {
     return (
       <div className="loginFormOutline">
         <div className="form">
           <div className="formElement">
             <label htmlFor="">Имя пользователя:</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={this.state.login}
+              onChange={this.onChangeHandler.bind(this, "login")}
+            />
           </div>
           <div className="formElement">
             <label htmlFor="">Пароль:</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={this.state.password}
+              onChange={this.onChangeHandler.bind(this, "password")}
+            />
           </div>
           <div className="formElement">
-            <button>Войти</button>
+            <button onClick={this.onSubmitHandler}>Войти</button>
           </div>
         </div>
       </div>
