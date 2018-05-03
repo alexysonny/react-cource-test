4f72f641f490c306e50ec29771fbd3921d69efd1
Aleksei Kostenko <alexysonny@gmail.com>
2018-05-03 11:19:54 +0300

    redirect while user not logged in

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 class Profile extends Component{
     render() {
+        if (!this.props.isLoggedIn) {
+            return(
+                <Redirect to={'/login'}/>
+            )
+        }
         return(
             <h1>Profile</h1>
         );
